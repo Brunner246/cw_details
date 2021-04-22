50d50620b7953c23851a2fe6ce9b4793b27bad03
AzureAD\MichaelBrunner <brunner@cadwork.swiss>
2021-04-22 08:29:08 +0200

    start file

---
+++ cw_details.py
@@ -0,0 +1,21 @@
+import attribute_controller as ac, element_controller as ec, cadwork as c, \
+    geometry_controller as gc
+
+
+
+def main():
+
+
+    return
+
+
+# =================================================
+
+def xy():
+    """
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
